5fc7f054d75eb115b7afca921d52bc62d2c7bc8a
Dino Becaj <dbecaj1@gmail.com>
2023-04-12 20:46:40 -0500

    added digitalocean app

--- src/api/stormApi.js
+++ src/api/stormApi.js
@@ -1,8 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const devBaseUrl = 'http://127.0.0.1:8000/'
+const prodBaseUrl = 'http://104.131.165.26:8000/'
+const baseUrl = prodBaseUrl
+
 export const stormApi = createApi({
   reducerPath: 'stormApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
       query: (data) => ({
